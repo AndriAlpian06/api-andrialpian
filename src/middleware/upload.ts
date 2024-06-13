0d179283a96ebc295d227a069956ea5d9ed6dc36
AndriAlpian06 <61491000+AndriAlpian06@users.noreply.github.com>
2024-06-13 14:02:05 +0700

    sixth commit

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -3,7 +3,8 @@ import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, process.env.UPLOAD_DIR || '../../uploads'))
+        cb(null, path.join(__dirname, '../../uploads' || process.env.UPLOAD_DIR))
+        console.log('Upload directory:', process.env.UPLOAD_DIR); 
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
